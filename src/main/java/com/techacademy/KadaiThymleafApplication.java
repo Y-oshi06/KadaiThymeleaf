4b6cc146f7558242265b0c2699edd29a55a3900f
Y-oshi06 <black.gorilla0930@gmail.com>
2024-06-01 16:41:17 +0900

    課題提出

---
+++ src/main/java/com/techacademy/KadaiThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiThymleafApplication.class, args);
+	}
+
+}
